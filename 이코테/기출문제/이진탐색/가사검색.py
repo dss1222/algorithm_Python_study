988c53d12188a910868df37086f9628671659782
신동석 <96603945+dss1222@users.noreply.github.com>
2022-05-02 21:41:59 +0900

    문제 추가

---
+++ 이코테/기출문제/이진탐색/가사검색.py
@@ -0,0 +1,6 @@
+def solution(words, queries):
+    answer = []
+    return answer
+
+
+print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"], ["fro??", "????o", "fr???", "fro???", "pro?"]))
