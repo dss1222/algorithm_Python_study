a4a6f409f68a8947944a2e8617512dd2c167f48d
신동석 <96603945+dss1222@users.noreply.github.com>
2022-05-03 21:59:10 +0900

    문제 추가

---
+++ 파이썬 알고리즘 인터뷰/6장 문자열 조작/1 리스트로 변환.py
@@ -0,0 +1,12 @@
+def isPalindrome(self, s : str) -> bool:
+    strs = []
+    for char in s:
+        if char.isalnum():
+            strs.append(char.lower())
+
+    while len(strs) > 1:
+        if strs.pop(0) != strs.pop():
+            return False
+
+
+    return True
