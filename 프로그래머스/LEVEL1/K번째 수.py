a622efb8e2e49b53377876364cc0afa61e6c2d1c
신동석 <96603945+dss1222@users.noreply.github.com>
2022-05-21 21:42:35 +0900

    문제 추가

---
+++ 프로그래머스/LEVEL1/K번째 수.py
@@ -0,0 +1,8 @@
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        i,j,k = command
+        answer.append(list(sorted(array[i-1:j]))[k-1])
+    return answer
+
+solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]])
