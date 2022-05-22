7688d3dd33bed8a62fabf45cca1fce338cb6fe86
신동석 <96603945+dss1222@users.noreply.github.com>
2022-05-22 19:32:07 +0900

    문제 추가

---
+++ 프로그래머스/LEVEL1/크레인 인형뽑기 게임.py
@@ -0,0 +1,20 @@
+def solution(board, moves):
+    stacklist = []
+    answer = 0
+
+    for i in moves:
+        for j in range(len(board)):
+            if board[j][i-1] != 0:
+                stacklist.append(board[j][i-1])
+                board[j][i-1] = 0
+
+                if len(stacklist) > 1:
+                    if stacklist[-1] == stacklist[-2]:
+                        stacklist.pop(-1)
+                        stacklist.pop(-1)
+                        answer += 2
+                break
+
+    return answer
+
+print(solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]],[1,5,3,5,1,2,1,4]))
