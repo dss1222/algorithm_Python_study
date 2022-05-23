b3391d8e369f4771c56ddb081bcb0bf80c674f26
신동석 <96603945+dss1222@users.noreply.github.com>
2022-05-23 14:40:12 +0900

    문제 추가

---
+++ 백준/하이커스 알고리즘 문제/1037 약수.py
@@ -0,0 +1,5 @@
+import math
+N = int(input())
+n = list(map(int, input().split()))
+
+print(min(n)*max(n))
---
+++ 백준/하이커스 알고리즘 문제/11866 요세푸스 문제 0.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+n, k = map(int, input().split())
+s = deque([i for i in range(1, n+1)])
+
+print('<', end='')
+while s:
+    for i in range(k - 1):
+        s.append(s[0])
+        s.popleft()
+    print(s.popleft(), end='')
+    if s:
+        print(', ', end='')
+print('>')
