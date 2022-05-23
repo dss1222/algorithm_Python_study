a6eca10629e5caf22f0b9f4a88506b7aa0feabe5
신동석 <96603945+dss1222@users.noreply.github.com>
2022-05-23 13:05:28 +0900

    문제 추가

---
+++ 백준/하이커스 알고리즘 문제/11279 최대 힙.py
@@ -0,0 +1,18 @@
+import sys
+import heapq
+
+n = int(sys.stdin.readline())
+heap = []
+
+for _ in range(n):
+    num = int(sys.stdin.readline())
+
+    if num == 0:
+        if heap:
+            print(heapq.heappop(heap)[1])
+        else:
+            print("0")
+    else:
+        heapq.heappush(heap, [-num, num])
+
+
