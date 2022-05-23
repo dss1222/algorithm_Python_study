6d078c3ce501eb5f10b6696a3e4e671969f3e6a6
신동석 <96603945+dss1222@users.noreply.github.com>
2022-05-23 13:05:35 +0900

    문제 추가

---
+++ 프로그래머스/LEVEL1/숫자 문자열과 영단어.py
@@ -0,0 +1,12 @@
+change = {'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4',
+          'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
+
+
+def solution(s):
+    for x in change:
+        s = s.replace(x, change[x])
+    return int(s)
+
+
+
+print(solution("one4seveneight"))
